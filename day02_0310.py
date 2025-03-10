8970bff73876985c48862d5360029e8327354943
jungjisu <jisu1008@inha.edu>
2025-03-10 12:04:27 +0900

    day02 v0.1 np

--- day02_0310.py
+++ day02_0310.py
@@ -1,22 +1,24 @@
-# import math
-# math.sin(2.1)*math.exp(-2)
+# 모든게 전부 object.
+# 모든 변수 이름을 attribute다.
+#numpy --> 수치해석학
+#matplot --> 눈으로 확인할 때 그림용.
+# numpy안에 통계처리 패키지도 있음.
+import numpy as np
+import matplotlib.pyplot as plt
 
-a = 'hello'
-b = 'world'
-c = a + ',' +  b # concatenation
-print(c)
-print(3*a)
-print(a*3)
+x = np.linspace(0,5,500) # 0하고 5를 포함해서 50개로 쪼개줘라
+y = np.exp(-x)*np.sin(2*np.pi*x)
 
-import os
-curr_dir = os.getcwd()
+plt.plot(x,y)
+plt.show()
 
-print(curr_dir)
-Z = 4
-if Z > 3:
-    print('OK')
-    print('Z > 3 is true')
-print('stop')
+
+
+
+
+
+
+
 
 
 
