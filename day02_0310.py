823612190435ae63584252193dc8c692703e8b6c
jungjisu <jisu1008@inha.edu>
2025-03-10 11:36:44 +0900

    day02 v0.0 curr_dir

--- day02_0310.py
+++ day02_0310.py
@@ -1,2 +1,23 @@
-print("Hello world")
-print(2**100)
+# import math
+# math.sin(2.1)*math.exp(-2)
+
+a = 'hello'
+b = 'world'
+c = a + ',' +  b # concatenation
+print(c)
+print(3*a)
+print(a*3)
+
+import os
+curr_dir = os.getcwd()
+
+print(curr_dir)
+Z = 4
+if Z > 3:
+    print('OK')
+    print('Z > 3 is true')
+print('stop')
+
+
+
+
