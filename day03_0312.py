a96254b49df90cadacd9a65e11258ff03fb3406f
jungjisu <jisu1008@inha.edu>
2025-03-12 11:37:50 +0900

    day03 v0.4 input

--- day03_0312.py
+++ day03_0312.py
@@ -1,9 +1,22 @@
-import os
+# import os
+#
+# print('Current working directory -->', os.getcwd())
+#
+# x = input('string = ')
+# print('Congratulations!' , x)
 
-print('Current working directory -->', os.getcwd())
+n = int(input('n = '))
+line1 = '+' + '-'*n + '+' + '-'*n + '+'
+line2 = '|' + ' '*n + '|' + ' '*n + '|'
 
-x = input('string = ')
-print('Congratulations!' , x)
+# print(line1, end='')  # ' ' --> empty string
+print(line1)
+print(line2)
+print(line2)
+print(line1)
+print(line2)
+print(line2)
+print(line1)
 
-
-
+# 키보드의 모든 키는 문자열이다.
+# \n : enter key string
