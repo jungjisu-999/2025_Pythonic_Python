905cb3a659e3fea2da14a131b74e1fd27c96351f
jungjisu <jisu1008@inha.edu>
2025-03-12 11:24:10 +0900

    day03 v0.3 input

--- day03_0312.py
+++ day03_0312.py
@@ -1,27 +1,9 @@
-#지각
-#ex1)
-import sys
-print(sys.platform) # print system platform
-print(2**100)
+import os
 
-x = '(pythonnic-python)'
-print(x*3)
-print( '[What a ]' +x+ '[ !]')
+print('Current working directory -->', os.getcwd())
 
-print(" -----")
-print("|     |")
-print("|     |")
-print(" -----")
+x = input('string = ')
+print('Congratulations!' , x)
 
-# ex2)
-m = 10
-lin1 = '+' + '-'*m + '+' + '-'*m + '+'
-lin2 = '|' + ' '*m + '|' + ' '*m + '|'
 
-print(lin1)
-print(lin2)
-print(lin1)
-print(lin2)
-print(lin1)
 
-#
