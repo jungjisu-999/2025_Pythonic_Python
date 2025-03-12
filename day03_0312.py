032aeee1063113cff8b40c58a0ecd7a3a429c699
jungjisu <jisu1008@inha.edu>
2025-03-12 12:02:03 +0900

    day03 v0.6 Unicode

--- day03_0312.py
+++ day03_0312.py
@@ -1,13 +1,5 @@
-# n = int( input('n = '))
-n = int( input('칸의개수 = '))
-m = int( input('줄의개수 = '))
-line1 = '+' + '-'*n + '+' + '-'*n + '+'
-line2 = '|' + ' '*n + '|' + ' '*n + '|'
+#unicode
 
+# print('\u263A'* 3)
+print('\U00010645'* 3)
 
-line2enter = line2 + '\n'
-lines = line1+ '\n' + line2enter*m + line1 + line2enter*m + line1
-print(lines)
-# 출력값 이상함.
-# \n : enter key string
-#Unicode
