226460b82e82ac84c9fe5ffe521caeeded3ca494
jungjisu <jisu1008@inha.edu>
2025-03-12 11:50:54 +0900

    day03 v0.5 input

--- day03_0312.py
+++ day03_0312.py
@@ -1,22 +1,13 @@
-# import os
-#
-# print('Current working directory -->', os.getcwd())
-#
-# x = input('string = ')
-# print('Congratulations!' , x)
-
-n = int(input('n = '))
+# n = int( input('n = '))
+n = int( input('칸의개수 = '))
+m = int( input('줄의개수 = '))
 line1 = '+' + '-'*n + '+' + '-'*n + '+'
 line2 = '|' + ' '*n + '|' + ' '*n + '|'
 
-# print(line1, end='')  # ' ' --> empty string
-print(line1)
-print(line2)
-print(line2)
-print(line1)
-print(line2)
-print(line2)
-print(line1)
 
-# 키보드의 모든 키는 문자열이다.
+line2enter = line2 + '\n'
+lines = line1+ '\n' + line2enter*m + line1 + line2enter*m + line1
+print(lines)
+# 출력값 이상함.
 # \n : enter key string
+#Unicode
