a714115d1806a5010c489ed280eee82339be17ae
jungjisu <jisu1008@inha.edu>
2025-03-12 12:13:10 +0900

    day03 v0.7 import

--- day03_0312.py
+++ day03_0312.py
@@ -1,5 +1,21 @@
-#unicode
+#import
 
-# print('\u263A'* 3)
-print('\U00010645'* 3)
+# import numpy
+# import numpy as np
+# from math import sin, cos, tan
+# def
+my_pi = 3.141592
+my_e = 2.71828
+
+def nt_fun( x, y):
+    value = x + y
+
+    return value
+class my_class:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print( self.x, '<-->', self.y)
 
