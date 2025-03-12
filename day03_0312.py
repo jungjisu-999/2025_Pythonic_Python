9fe3ef7cfa3aa2daeb49fa9becc8d3f4df18a50b
jungjisu <jisu1008@inha.edu>
2025-03-12 11:13:10 +0900

    day03 v0.2 built

---
+++ day03_0312.py
@@ -0,0 +1,27 @@
+#지각
+#ex1)
+import sys
+print(sys.platform) # print system platform
+print(2**100)
+
+x = '(pythonnic-python)'
+print(x*3)
+print( '[What a ]' +x+ '[ !]')
+
+print(" -----")
+print("|     |")
+print("|     |")
+print(" -----")
+
+# ex2)
+m = 10
+lin1 = '+' + '-'*m + '+' + '-'*m + '+'
+lin2 = '|' + ' '*m + '|' + ' '*m + '|'
+
+print(lin1)
+print(lin2)
+print(lin1)
+print(lin2)
+print(lin1)
+
+#
