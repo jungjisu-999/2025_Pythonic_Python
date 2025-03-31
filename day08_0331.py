ce4ebcb4ef9fe5a936151ee8721bcf497d178d69
jungjisu <jisu1008@inha.edu>
2025-03-31 11:03:06 +0900

    day08 v1.2 stringmethod

---
+++ day08_0331.py
@@ -0,0 +1,13 @@
+#String Methods(1/5)
+# S = 'pingpong'
+#
+# # splitting a string('...')
+# #into list([...]) of characters
+# L = list(S)
+# print('(1)', L)
+
+
+L = []
+for k in range(10):
+    L.append( k )
+print( L )
