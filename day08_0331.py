deefbe6a5ec37f1030b71e1be16bd15d5e5dbba7
jungjisu <jisu1008@inha.edu>
2025-03-31 11:26:46 +0900

    day08 v1.3 cut by find

--- day08_0331.py
+++ day08_0331.py
@@ -1,13 +1,27 @@
-#String Methods(1/5)
-# S = 'pingpong'
-#
-# # splitting a string('...')
-# #into list([...]) of characters
-# L = list(S)
-# print('(1)', L)
+def cut_by_piece(S, piece, n_max ):
+    L_piece = len(piece)
+    L_S = len( S )
+
+    found = []
+
+    for ith in range( L_S ) :
+        k = S.find(piece )
+        if k == -1:
+            found.append( len(S))
+            return found
+
+        found.append( k )
+        S = S[k+L_piece:]
+
+    return found
+def find_all(S, piece, n_max):
+    res = cut_by_piece( S, piece, n_max)
+    return res
 
 
-L = []
-for k in range(10):
-    L.append( k )
-print( L )
+if __name__=='__main__':
+    S_given = '1234XXX56XXX7890123XXX98765'
+    result = find_all( S_given, 'XXX', 100)
+    print( result)
+
+
