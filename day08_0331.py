7c4c2fde1c03bf08558d533e47a6e27a65e5e596
jungjisu <jisu1008@inha.edu>
2025-03-31 22:39:46 +0900

    v1.5 Hw2

--- day08_0331.py
+++ day08_0331.py
@@ -1,28 +1,45 @@
-S = ' Straw.Berry is good\n '
+import random
 
-print(' (1) print(S)')
-print('['+S+']')
-print( '(2) print( repr(S) )')
-print('[' + repr(S)+ ']')
 
-#String: formatting Methods(3/5)
+def touch(a, b, position, n_moves):
+    current = position
 
-S0 = 'It is very nice to learn PYthon.'
-print('S0 =', S0)
-S1 = 'It %s very %s to learn %s.' % ('is', 'nice','Python')
-print('S1 =', S1)
+    for k in range( n_moves ):
+        current += random.choice([-1, 1])
+        if current == a or current == b:
+            return current
 
-S2 = 'It {} very {} to learn {}.'.format('is','nice','Python')
-print('S2 =', S2)
-S3 = 'It {0} very {1} to arn {2}.'.format('is','nice', 'Python')
-print('S3 =', S3)
-S4 = 'It {2} very {1} to learn {0}.'.format('is', 'nice','Python')
-print('S4 =', S4)
+    return current
 
-try:
-    S_err = 'It {0} very {1} to learn {4}.'.format('is', 'nice', 'Python')
-    print('S_err=', S_err)
-except IndexError as e:
-    print( e )
+def show_result( a, b, trials, a_touches, b_touches ):
+    a_prob = a_touches/trials
+    b_prob = b_touches/trials
+
+    print('+' + '-'*5 + '+' + '-'*19 + '+' + '-'*13 + '+')
+    print('|' + ' end ' + '|' + f'trials={trials:12d}' + '|' + ' probability ' + '|')
+    print('+' + '-'*5 + '+' + '-'*19 + '+' + '-'*13 + '+')
+    print('|' + f'{a:5d}' + '|' + f'{a_touches:19d}' + '|' + f'{a_prob:13.4f}' + '|')
+    print('|' + f'{b:5d}' + '|' + f'{b_touches:19d}' + '|' + f'{a_prob:13.4f}' + '|')
+    print('+' + '-'*5 + '+' + '-'*19 + '+' + '-'*13 + '+')
+
+if __name__ == '__main__':
+    a = eval(input('a = '))
+    b = eval(input('b = '))
+    initial = eval(input('initial position = '))
+    n_steps = eval(input('maximum steps = '))
+    m_trials = eval(input('how many trials = '))
+
+    a_touch = 0
+    b_touch = 0
+
+    for trial in range(m_trials):
+        final = touch(a, b, initial, n_steps)
+        if final == a:
+            a_touch += 1
+
+        if final == b:
+            b_touch += 1
+
+    show_result(a, b, m_trials, a_touch, b_touch)
 
 
