798ec0126a78503109dfe0225ef60ca0563747ca
jungjisu <jisu1008@inha.edu>
2025-04-02 11:37:12 +0900

    day09 v1.9 Mort String

--- day09_0402.py
+++ day09_0402.py
@@ -1,28 +1,8 @@
 #String: Pattern Matching Methods_Lecture 2
 
-import re
-S = '/usr/home:myname'
+S = 'Saint 24 John 07 makes me cloud 9.'
+tbl = str.maketrans('mSao', 'eJot', '0123456789')
+SS = S.translate( tbl )
+print(SS)
 
-mysplit1 = re.split('[/]', S)
-# mysplit1_1 = re.split(' [/] ', S)
-print( '(split-1)', mysplit1 )
-# print( '(split-1)', mysplit1_1 )
-#질문
-
-mysplit2 = re.split('[:]', S)
-print( '(split-2)', mysplit2 )
-mysplit3 = re.split('[/:]', S)
-print( '(split-3)', mysplit3 )
-
-mymatch1 = re.match('(.*)[/:]', S)
-print( '(match-1)', mymatch1.groups())
-mymatch2 = re.match('[/:](.*)', S)
-print( '(match-2)', mymatch2.groups())
-mymatch3 = re.match('(.*)[/:](.*)', S)
-print( '(match-3)', mymatch3.groups())
-mymatch4 = re.match('[/:](.*)[/:]', S)
-print( '(match-4)', mymatch4.groups())
-mymatch5 = re.match('[/:](.*)[/:](.*)[/:](.*)', S)
-print( '(match-5)', mymatch5.groups())
-mymatch6 = re.match('(.*)[/:](.*)[/:](.*)[/:](.*)', S)
-print( '(match-6)', mymatch6.groups())
+# isspace(), isdigit(), isnumeric()
