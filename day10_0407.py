861fbf37a41da5446826dd957c5d776a50ab8580
jungjisu <jisu1008@inha.edu>
2025-04-07 11:31:04 +0900

    day10 v2.3 list_mutable

--- day10_0407.py
+++ day10_0407.py
@@ -1,15 +1,31 @@
 #List / mutable
-#my_list_setitem.py
 
-L = ['a', 'b', 'c', 'd', 'e']
+def get_entry( mat, i_row, j_col) :
+    try:
+        row_pick = mat[i_row]
+        return row_pick[j_col] if j_col < len(row_pick) else 0.0
+    except IndexError:
+        return 0.0
 
-print('L =', L )
+def show_matrix( mat, words ):
+    size_row = len(mat)
+    size_col = max( [len(row) for row in mat])
 
-L[1:2] = [1,2,3]
-print('L =', L)
+    print(words)
+    for i in range(size_row):
+        entries = [ '{:12.4f}'.format(get_entry(mat, i, j))
+                    for j in range(size_col)]
+        print( ' '.join(entries) )
 
-L[5:] = [10,20,30]
-print('L =', L)
+#List is able to describe a matrix
+M = [ [1, 2, 3], [4, 5], [7] ]
+show_matrix( M, '(1) M -->')
+print('(2) M[0] -->', M[0])
+print('(3) M[0][2] -->', M[0][2])
 
-L[2:4] = ['apple', 'mango', 'lemon']
-print('L =', L)
+#List is mutable
+M[1] = [40,50,60]
+show_matrix( M, '(4) M -->')
+
+col_1 = [ get_entry(M,i,1) for i in range(len(M))]
+print('(5) 1st column of M -->', col_1)
