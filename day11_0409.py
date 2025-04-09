4a60c47176cc5bc9a5bce8aff7a7fd4e8411ee5b
jungjisu <jisu1008@inha.edu>
2025-04-09 11:22:23 +0900

    day10 v2.6 np pk(2/3)

--- day11_0409.py
+++ day11_0409.py
@@ -1,31 +1,28 @@
-# numpy package(1/3)
+# numpy package(2/3)
 
 import numpy as np
 
-matA = [ [1,2,3], [-1,0,1], [0,1,1] ] # 3*3 matrix
-matB = [ [1,1,1], [1,1,-2], [5,0,1] ]
+seq_lin = np.linspace(0, 1, 10)
 
-u_vec = [-1,1,2] # 1*3 matrix
-v_vec = [1,-3,0]
+print('(1) make linear sequence:')
+print('seq_lin)=', seq_lin )
+print('type(seq_lin) =', type(seq_lin))
+print('seq_lin[3:8] =', seq_lin[3:8])
 
-#numpy array from numeric list
-u_vec_np = np.array(u_vec)
-v_vec_np = np.array(v_vec)
+mat = np.array([ [ x**n for n in range(5)] for x in range(1,4)])
+print(mat)
+print(mat[2:1])
+print( mat[2:,1:3]) # 질문
+print(mat.T)   # 질문 : T가 mat의 attribute인지 / __setattr__ / class때 배울 예정
 
-matA_np = np.array(matA)
-matB_np = np.array(matB)
+# A.T : A에는 T라는 attribute가 정의 되어 있다.
+# attribute : 함수 name이다.
 
-print('(1) u_vec_np =', u_vec_np, 'v_vec_np =', v_vec_np)
-print('(addition) u_vec_np + v_vec_np =', u_vec_np + v_vec_np)
-print('(scalar multiplication) 0.5*matA_np =', 0.5**u_vec_np)
+mat[:2,1:4] = np.array( [ [10,20,30], [-10,-20,-30]])
+#Or, mat[:2,1:4] = [[ 10,20,30], [-10,-20,-30]]
+print(mat)
+if type(mat) == list:
+    print('(4) mat is a list.')
+else:
+    print('(5) mat is not a list: type(mat)=', type(mat))
 
-print( '(2-A) matA_np\n', matA_np )
-print( '(2-B) matB_np\n', matB_np )
-print('(addition) matA_np + matB_np\n', matA_np + matB_np)
-print('(scalar multiplication) matA_np matB_np\n', 0.5*matA_np)
-
-print('(matrix multiplication) matA_np matB)np\n', matA_np@matB_np)
-
-inv_matA_np = np.linalg.inv( matA_np )
-print('(matrix inverse) the inverse matrix of matA_np\n', inv_matA_np)
-print('(confirm inverse) matA_np inv_matB_np\n', matA_np@inv_matA_np)
